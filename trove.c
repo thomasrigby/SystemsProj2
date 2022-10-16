0aa4827ef67d0f8dad5b33bf4afe408a560f786e
thomas rigby <tomr0607@hotmail.com>
2022-10-16 14:34:55 +0800

    hola

--- trove.c
+++ trove.c
@@ -1,5 +1,6 @@
 //Project 2 which creates a command line utility program 
 //to build, find and compress files 
+#define POSIX_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,68 +10,77 @@
 #include <dirent.h>
 
 
-//report the program's synopsis, should a command-line process detect errors
-// int usage()
-// {
-// }
-
-//Searching through directory to find file
-int findFile(char *fileName)
-{  
-  return 0;
-}
-
-//Building a file 
-int buildFile()
+void trovePrint(LIST *list)
 {
-  return 0;
+    while(list != NULL){
+        printf("%s\n", list->s);
+        list = list->follow;
+    }
 }
 
-//Modifying the file 
-int modifyFile()
+void makeFileList(char *argv[])
 {
-  return 0;
-}
+    struct dirent *files;
+    DIR *file_list;
+    LIST *mylist;
+
+
+    file_list = opendir(".");  
+    if(file_list == NULL)
+    {
+        printf("Unable to open directory");
+        exit(0); 
+    }
+    else
+    {
+       printf("Directory opened!\n");
+        while ((files = readdir(file_list)) != NULL)
+                {
+                    //printf("[%s]\n", files->d_name);
+                    mylist = list_add(mylist, files->d_name);
+                }     
+                trovePrint(mylist);
+                     
+        }
+        closedir(file_list);  
+    }
 //main function to receive and validate the command-line options and arguments
 //command line format is  ./trove  [-f trovefile]  word 
 // command line format is ./trove  [-f trovefile]  [-b  |  -r  |  -u]  [-l length]  filelist
 int main(int argc, char *argv[])
 {
-  if(argc < 3) //3 arguments required to find file 
-  {
-    printf("Not enough arguments to find file\n");
-    exit(0);
-  } 
-  else if (argc == 3 && strcmp(argv[1], "-f") == 0) // ensures second argument is to find file
-  {
-    if(strlen(argv[2]) < 3) //minimum length of the word is 3 characters
-    {
-        printf("Word has to be minimum of 3 characters\n");
-        exit(0);
-      }
-    else{
-    printf("Finding file to create %s file\n", argv[2]);
-  }
-}
-  else if (argc != 7)
-  {
-    printf("Not enough arguments\n");
-  }
-  else if((strcmp(argv[1],"-f") == 0))
-    {
-      if((strcmp(argv[3], "-b"))==0){
-        printf("Build a file named %s \n", );
-      }
-      if((strcmp(argv[3], "-r"))==0){
-        printf("Removing a file from filelist\n");
-      }
-      if((strcmp(argv[3], "-u"))==0){
-        printf("Update contents of trove-file with filelist contents\n");
-      }
-    }
-    else{
-        printf("Invalid argument, please use -f && -b/-r/-u\n");
-    }
+//   if(argc < 3) //3 arguments required to find file 
+//   {
+//     printf("Not enough arguments to find file\n");
+//     exit(0);
+//   } 
+//   else if (argc == 3 && strcmp(argv[1], "-f") == 0) // ensures second argument is to find file
+//   {
+//     if(strlen(argv[2]) < 3) //minimum length of the word is 3 characters
+//     {
+//         printf("Word has to be minimum of 3 characters\n");
+//         exit(0);
+//       }
+//     else{
+//     printf("Finding file to create %s file\n", argv[2]);
+//   }
+//   }
+ 
+//   else if((strcmp(argv[1],"-f") == 0))
+//     {
+//       if((strcmp(argv[3], "-b"))==0){
+//         printf("Build a file named \n");
+//       }
+//       if((strcmp(argv[3], "-r"))==0){
+//         printf("Removing a file from filelist\n");
+//       }
+//       if((strcmp(argv[3], "-u"))==0){
+//         printf("Update contents of trove-file with filelist contents\n");
+//       }
+//     }
+//     else{
+//         printf("Invalid argument, please use -f && -b/-r/-u\n");
+//     }
 
 //checking if third arg is a word
   if(isalpha(atoi(argv[2])) != 0)
@@ -78,4 +88,8 @@ int main(int argc, char *argv[])
     printf("File name should not contain digits!\n");
     exit(0);
   }
+  
+  makeFileList(&argv[1]);
+
+  return 0;
 }
--- trove.h
+++ trove.h
@@ -1,14 +1,38 @@
-//header file to define structs to store directories and values 
+
+//creating a Hashtable that stores the valyes of the files in the filelist 
+//for the trove file to traverse through and determine x/y/z
+//Hashtable will contain the elements of a filelist that meet a criteria 
+//Return the filename and the pathname 
+
+#define _POSIX_C_SOURCE  200809L
+
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-//creating a construct that stores the valyes of the files in the filelist 
-//for the trove file to traverse through and determine x/y/z
-struct filelist
+
+typedef struct _list{
+    char           *s;
+    struct _list   *follow;
+} LIST; 
+
+LIST *list_new(void)
 {
-    char name; 
-    int nameLength; 
-    char pathname;
-    char directory;
-};
+    return NULL;
+}
+
+LIST *list_add(LIST *oldlist, char *s)
+{
+    LIST *newlist = malloc(sizeof(LIST));
+    //check....
+
+    newlist->s = strdup(s);
+    //check....
+    
+    newlist->follow = oldlist; 
+
+    return newlist; 
+}
